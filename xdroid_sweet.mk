32379f71a3467bde24be5475489cee49700a2b09
shashank1439 <9945shashank@gmail.com>
2023-01-01 05:39:02 +0530

    Revert "sweet: xdroid add support for face unlock"
    This reverts commit 8b06f2bf4ae5f63c8031664f9ff2cec55576dfda.

--- xdroid_sweet.mk
+++ xdroid_sweet.mk
@@ -19,7 +19,6 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := xdroid_sweet
