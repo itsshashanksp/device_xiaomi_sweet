372b60c7492adabf0d8fb9d177a02307a499f2a8
itsshashanksp <9945shashank@gmail.com>
2024-07-23 10:58:55 +0530

    sweet: Initialize PixelStar
    Signed-off-by: Shashank Patil <9945shashank@gmail.com>

--- pixelstar_sweet.mk
+++ pixelstar_sweet.mk
@@ -11,14 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixelstar stuff.
+$(call inherit-product, vendor/pixelstar/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := pixelstar_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Pixelstar Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_BLUR_RECENT := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+USE_PIXEL_CHARGER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+TARGET_SUPPORTS_NOW_PLAYING := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
